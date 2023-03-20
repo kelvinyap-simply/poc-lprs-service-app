bd43865df7331ae1ec0d8e29c9b90fa9cc8d8641
Kelvin Yap <kelvin.yap@simplysakal.asia>
2023-03-20 10:09:28 +0800

    Initial commit

---
+++ src/main/java/com/simply/poclprsserviceapp/PocLprsServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.simply.poclprsserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocLprsServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLprsServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simply/poclprsserviceapp/PocLprsServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simply.poclprsserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocLprsServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
