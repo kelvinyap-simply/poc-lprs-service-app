732c483f5e130f88b0b94307a4736754b032173e
Kelvin Yap <kelvin.yap@simplysakal.asia>
2023-03-20 10:15:44 +0800

    Add DI

--- src/main/java/com/simply/poclprsserviceapp/controller/LprsController.java
+++ src/main/java/com/simply/poclprsserviceapp/controller/LprsController.java
@@ -1,5 +1,6 @@
 package com.simply.poclprsserviceapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LprsController {
+
+    @Autowired
+    public LprsController() {}
 
     //@GetMapping("/hello")
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
