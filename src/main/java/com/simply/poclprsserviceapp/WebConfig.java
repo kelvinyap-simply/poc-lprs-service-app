e56b727be08c9292be11181cea4dafd9b4b86685
Kelvin Yap <kelvin.yap@simplysakal.asia>
2023-03-20 10:51:36 +0800

    add web config

--- src/main/java/com/simply/poclprsserviceapp/WebConfig.java
+++ src/main/java/com/simply/poclprsserviceapp/WebConfig.java
@@ -1,2 +1,7 @@
-package com.simply.poclprsserviceapp;public class WebConfig {
+package com.simply.poclprsserviceapp;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
 }
